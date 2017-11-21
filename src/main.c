f2ccf0a80d49bbfe914cd086506e39ee09c3466c
petersondb <petersonbardelli@gmail.com>
2017-11-21 21:43:49 -0200

    oi

--- src/main.c
+++ src/main.c
@@ -6,17 +6,42 @@
  */
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main() {
 
-  char c;
+	char frase[200];
+	char aux;
+	int i = 0, tam, j, words = 0, indicador = 0;
+	frase[0] = 'c';
+	while (aux != '\n') {	 
+    	scanf ("%c", &frase[i]);
+    	aux = frase[i];
+    	i++;
+ 	}
 
-  c = 1;
-
-  while (c != '\n') {
-    scanf("%c", &c);
-  }
-
-  printf("1\n");
+ 	for(j = 0; j < i - 1; j++) {
+ 		if ((frase[j] != ' ') && (frase[j] != ',') && (frase[j] != '.') && (indicador == 0) && (frase[j] != '-') && (frase[j] != '!') ) {
+ 			words++;
+ 			indicador++;
+ 		} else {
+ 			if ((frase[j] != ' ') && (frase[j] != ',') && (frase[j] != '.') && (frase[j] != '-') && (frase[j] != '!')) {
+ 				indicador = 1;	
+ 			} else {
+ 				indicador = 0;
+ 			}
+ 		}
+ 	}
+  printf("%d\n", words);
   return 0;
 }
+
+/* if ((estado == 0) && (((z >= '0') && (z<='9')) || ((z>='a') && (z<='z')) || ((z>='A') && (z<='Z')) || (z==',') || (z=='.') || (z==':'))){
+	p++;		
+	*/
+
+/*  
+   
+*/
+
